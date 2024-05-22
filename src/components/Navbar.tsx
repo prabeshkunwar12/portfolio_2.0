29b3123fe9e901ca6661b3cbcd141fd8d963d2f9
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-05-22 17:44:10 -0300

    custom darkmode toggler added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
+'use client'
+
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { navLinks } from "@/lib/constants"
-import { buttonVariants } from "./ui/button"
-import { Braces, Code, Github, Linkedin } from "lucide-react"
+import { Button, buttonVariants } from "./ui/button"
+import { Braces, Code, Github, Linkedin, Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
 
 const Navbar = () => {
+    const { setTheme, resolvedTheme } = useTheme()
+    console.log(resolvedTheme)
     return (
         <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
@@ -13,25 +18,23 @@ const Navbar = () => {
                         <Link href="/" className="flex z-40 font-semibold text-blue-600">
                             {"<"}<span className="text-green-600">PrabeshNarsinghKunwar</span>&nbsp;{"/>"}
                         </Link>
-                        <div className="flex items-center">
-                            <ul className="flex gap-x-2">
-                                {navLinks.map((link) => (
-                                    <li key={link.id}>
-                                        <Link 
-                                            className={buttonVariants({
-                                                variant: "code",
-                                                size: "code"
-                                            })}
-                                            href={`#${link.id}`}
-                                        >
-                                            {link.title}
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                        <ul className="hidden lg:flex items-center gap-x-2">
+                            {navLinks.map((link) => (
+                                <li key={link.id}>
+                                    <Link 
+                                        className={buttonVariants({
+                                            variant: "code",
+                                            size: "code"
+                                        })}
+                                        href={`#${link.id}`}
+                                    >
+                                        {link.title}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                    <ul className="flex">
+                    <ul className="hidden lg:flex">
                         <li>
                             <Link 
                                 className={buttonVariants({
@@ -75,6 +78,25 @@ const Navbar = () => {
                             >
                                 <Braces />
                             </Link>
+                        </li>
+                        <li>
+                            {resolvedTheme==="light" ? (
+                                <Button
+                                    variant={'code'}
+                                    size={'code'}
+                                    onClick={() => setTheme("dark")}
+                                >
+                                    <Moon />
+                                </Button>
+                            ):(
+                                <Button
+                                    variant={'code'}
+                                    size={'code'}
+                                    onClick={() => setTheme("light")}
+                                >
+                                    <Sun />
+                                </Button>
+                            )}
                         </li>
                     </ul>
                 </div>
