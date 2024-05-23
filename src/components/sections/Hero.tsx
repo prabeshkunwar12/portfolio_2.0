4a2152dee4a3e945fe2e433298624951b94277c9
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-05-22 21:22:32 -0300

    responsive hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -4,15 +4,17 @@ import MaxWidthWrapper from '../MaxWidthWrapper'
 const Hero = () => {
 	return (
 		<MaxWidthWrapper>
-			<div className='flex flex-col h-[calc(100vh-3.5rem)] justify-center -my-24 gap-y-4'>
-				<h1 className=' font-bold text-xl text-left text-zinc-600 dark:text-zinc-300'>
-					<span className='text-green-600 dark:text-green-300'>let</span> 
-					{" "}prabesh 
-					<span className='text-primary'>{" ="}</span>
-				</h1>
-				<span className=' font-bold text-7xl text-primary'>
-					{"<Web Developer />"}
-				</span>
+			<div className='flex flex-col h-[calc(100vh-3.5rem)] mx-auto justify-center md:items-center -my-24 gap-y-4'>
+				<div>
+					<h1 className=' font-bold text-xl text-left text-zinc-600 dark:text-zinc-300'>
+						<span className='text-green-600 dark:text-green-300'>let</span> 
+						{" "}prabesh 
+						<span className='text-primary'>{" ="}</span>
+					</h1>
+					<span className=' font-bold text-4xl sm:text-6xl md:text-7xl text-primary'>
+						{"<Web Developer />"}
+					</span>
+				</div>
 			</div>
 		</MaxWidthWrapper>
 	)
